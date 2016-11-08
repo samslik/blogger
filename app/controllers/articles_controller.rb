d49278c8299fadf86c53a4adcf8a7d4a56644937
Anna <samslik@gmail.com>
2016-11-08 13:11:40 +0100

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
   end
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
   def create
     @article = Article.new(article_params)
